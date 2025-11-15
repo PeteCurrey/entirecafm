6ff8ab76da3fe6abd0ce8a894bd2a5815a726ec9
Peter Currey <pete@pljsgroup.com>
2025-11-15 06:30:42 +0000

    File changes

--- src/components/marketing/SocialAutomationTab.jsx
+++ src/components/marketing/SocialAutomationTab.jsx
@@ -1,6 +1,7 @@
+
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
-import { useQuery, useMutation, useQueryClient } from "@tantml:react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { 
   Share2, 
   Calendar,
